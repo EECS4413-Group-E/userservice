8f97899a3431adbe22a9c63d1b1dc332a03f43b9
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2026-06-20 21:22:28 -0400

    Generated project with spring initializr

---
+++ src/main/java/com/eecs4413/groupe/userservice/UserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eecs4413.groupe.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+}
